552b914e46af4abb96274c0c28b5942b5c8b5946
Martijn van Es <martijn.es@gmail.com>
2024-01-10 20:30:17 +0100

    Start of Advent of Code project and project preparation

--- src/test/java/puzzles/TestCaseAOC2015.java
+++ src/test/java/puzzles/TestCaseAOC2015.java
@@ -3,17 +3,26 @@ package puzzles;
 import org.junit.jupiter.api.Test;
 import util.FileHelper;
 
-import static util.StringHelper.countOccurencesOfCharacterInString;
+import static util.StringHelper.countOccurrencesOfCharacterInString;
+import static util.StringHelper.findPositionForThreshold;
 
 public class TestCaseAOC2015 {
     @Test
     public void dayOnePuzzleOne() {
         int answer;
-        String input = FileHelper.readFileToString("2015-day01-01.txt");
+        String input = FileHelper.readFileToString("2015-day01.txt");
 
-        int countUp = countOccurencesOfCharacterInString('(', input);
-        int countDown = countOccurencesOfCharacterInString(')', input);
+        int countUp = countOccurrencesOfCharacterInString('(', input);
+        int countDown = countOccurrencesOfCharacterInString(')', input);
         answer = countUp - countDown;
         System.out.println("The answer to the advent of code 2015 challenge day one puzzle one is: " + answer);
+    }
+
+    @Test
+    public void dayOnePuzzleTwo() {
+        int answer;
+        String input = FileHelper.readFileToString("2015-day01.txt");
+        answer = findPositionForThreshold('(', ')', input, -1) + 1;
+        System.out.println("The answer to the advent of code 2015 challenge day one puzzle two is: " + answer);
     }
 }
--- src/test/java/util/StringHelper.java
+++ src/test/java/util/StringHelper.java
@@ -1,9 +1,12 @@
 package util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Objects;
 
+@Slf4j
 public class StringHelper {
-    public static int countOccurencesOfCharacterInString(char character, String inputString) {
+    public static int countOccurrencesOfCharacterInString(char character, String inputString) {
         int count = 0;
         for (int i = 0; i < Objects.requireNonNull(inputString).length(); i++) {
             if (inputString.charAt(i) == character) {
@@ -11,5 +14,21 @@ public class StringHelper {
             }
         }
         return count;
+    }
+
+    public static int findPositionForThreshold(char charUp, char charDown, String inputString, int threshold) {
+        int count = 0;
+        int i;
+        for (i = 0; i < Objects.requireNonNull(inputString).length(); i++) {
+            if (inputString.charAt(i) == charUp) {
+                count++;
+            } else if (inputString.charAt(i) == charDown) {
+                count--;
+            } else log.error("Unexpected character " + inputString.charAt(i));
+            if (count == threshold) {
+                break;
+            }
+        }
+        return i;
     }
 }
