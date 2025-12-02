96359e426089cb8f0aadb929f7dd31ca4109066c
martijnvanes <martijn.es@gmail.com>
2025-12-02 08:11:24 +0100

    Day1 puzzle 1

--- src/test/java/util/Calculator.java
+++ src/test/java/util/Calculator.java
@@ -1,14 +1,32 @@
 package util;
 
+import lombok.experimental.UtilityClass;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@UtilityClass
 public class Calculator {
-    public static BigDecimal sumOfBigDecimalInList(List<BigDecimal> bigDecimalList) {
+    public BigDecimal sumOfBigDecimalInList(List<BigDecimal> bigDecimalList) {
         BigDecimal answer = BigDecimal.ZERO;
         for (BigDecimal bigDecimal : bigDecimalList) {
             answer = answer.add(bigDecimal);
         }
         return answer;
+    }
+
+    public int calc(int lowerLimit, int upperLimit, int startValue, int delta, String direction) {
+        int calculatedValue;
+        if (direction.equals("left")) {
+            calculatedValue = startValue - delta;
+        } else {
+            calculatedValue = startValue + delta;
+        }
+        if (calculatedValue < lowerLimit) {
+            calculatedValue = calculatedValue + upperLimit;
+        } else if (calculatedValue > upperLimit) {
+            calculatedValue = calculatedValue - upperLimit;
+        }
+        return calculatedValue;
     }
 }
